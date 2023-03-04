dd86938106e26cb0ad1c351a0f78746acd9e265e
Richard Shadrach <rhshadrach@gmail.com>
2023-03-04 16:55:25 -0500

    build exe fixup

--- make_exe.bat
+++ make_exe.bat
@@ -1,17 +1,13 @@
 @echo off
 
 set /p version=<center_of_blob/VERSION
-for /F "tokens=* USEBACKQ" %%F in (`where python`) do (
+for /F "tokens=* USEBACKQ" %%F in (`python -c 'import site; print(site.getsitepackages()[0])'`) do (
 set python_path=%%F
-)
-SETLOCAL
-FOR %%i IN ("%python_path%") DO (
-ECHO python_path=%%~pi
 )
 
 pyinstaller.exe^
  --add-data "center_of_blob/VERSION;center_of_blob"^
- --add-binary "%python_path%Lib/site-packages/Shapely.libs;Shapely.libs"^
+ --add-binary "%python_path%Shapely.libs;Shapely.libs"^
  --hidden-import range_slider^
  --exclude-module _bootlocale^
  --onefile center_of_blob/main.py^
