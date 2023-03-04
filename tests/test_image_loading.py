8e80d8e8b5a80c4127d62b559f25c5d5f08b2e1d
Richard Shadrach <rhshadrach@gmail.com>
2023-03-04 16:50:38 -0500

    test fixup

--- tests/test_image_loading.py
+++ tests/test_image_loading.py
@@ -1,15 +1,17 @@
-from tests import actions
+from tests import actions, data
 
 
 def test_load_image(qtbot, monkeypatch):
+    filename = "data/sample.tif"
     main = actions.setup_test(qtbot)
-    actions.load_image(monkeypatch, qtbot, main, "data/sample.tif")
+    actions.load_image(monkeypatch, qtbot, main, filename)
     assert main.has_img
-    assert main.filename == "/home/richard/dev/center_of_blob/tests/data/sample.tif"
+    assert main.filename == data.resolve_path(filename)
     assert len(main.centers) == 0
 
 
 def test_failed_image(qtbot, monkeypatch):
+    filename = "data/sample.tif"
     main = actions.setup_test(qtbot)
 
     actions.setup_close_message_box(qtbot)
@@ -17,9 +19,9 @@ def test_failed_image(qtbot, monkeypatch):
         actions.load_image(monkeypatch, qtbot, main, "invalid_file.png")
     assert main.filename is None
 
-    actions.load_image(monkeypatch, qtbot, main, "data/sample.tif")
-    assert main.filename == "/home/richard/dev/center_of_blob/tests/data/sample.tif"
+    actions.load_image(monkeypatch, qtbot, main, filename)
+    assert main.filename == data.resolve_path(filename)
 
     with actions.setup_close_message_box(qtbot):
         actions.load_image(monkeypatch, qtbot, main, "invalid_file.png")
-    assert main.filename == "/home/richard/dev/center_of_blob/tests/data/sample.tif"
+    assert main.filename == data.resolve_path(filename)
